b0e53804e173961705bbfc00640b7e2a8bfc8c58
Christopher Wong <cw30355@gmail.com>
2024-01-09 17:26:56 -0500

    added in post, patch, and delete requests to app.py as needed

--- server/app.py
+++ server/app.py
@@ -3,7 +3,7 @@
 # Standard library imports
 
 # Remote library imports
-from flask import request, Flask, make_response
+from flask import request, Flask, make_response, jsonify, session
 from flask_restful import Resource, Api
 from flask_migrate import Migrate
 from flask_cors import CORS
@@ -23,6 +23,17 @@ class Users(Resource):
         users = User.query.all()
         users_dict_list = [user.to_dict() for user in users]
         return make_response(users_dict_list, 200)
+    
+    def post(self):
+        data=request.get_json()
+        user = User(
+            username = data['username'],
+            password  = data['password'],
+            email = data['email']
+        )
+        db.session.add(user)
+        db.session.commit()
+        return make_response(user.to_dict(), 201)
 api.add_resource(Users, '/users')
 
 class UsersById(Resource):
@@ -31,6 +42,25 @@ class UsersById(Resource):
         if not user:
             return make_response({'error': 'User not found'}, 404)
         return make_response(user.to_dict(), 200)
+    
+    def patch(self, id):
+        user = User.query.filter_by(id=id).first()
+        if not user:
+            return make_response({'error': 'User not found'}, 404)
+        data = request.get_json()
+        for key in data.keys():
+            setattr(user, key, data[key])
+        db.session.add(user)
+        db.session.commit()
+        return make_response(user.to_dict(), 200)
+    
+    def delete(self, id):
+        user = User.query.filter_by(id=id).first()
+        if not user:
+            return make_response({'error': 'User not found'}, 404)
+        db.session.delete(user)
+        db.session.commit()
+        return make_response({'message': 'User deleted'}, 204)
 api.add_resource(UsersById, '/users/<int:id>')
 
 class Collections(Resource):
@@ -38,6 +68,15 @@ class Collections(Resource):
         collections = Collection.query.all()
         collections_dict_list = [collection.to_dict() for collection in collections]
         return make_response(collections_dict_list, 200)
+    def post(self):
+        data = request.get_json()
+        collection = Collection(
+            name = data['name'],
+            user_id = data['user_id']
+        )
+        db.session.add(collection)
+        db.session.commit()
+        return make_response(collection.to_dict(), 201)
 api.add_resource(Collections, '/collections')
 
 class CollectionById(Resource):
@@ -46,6 +85,25 @@ class CollectionById(Resource):
         if not collection:
             return make_response({'error': 'Collection not found'}, 404)
         return make_response(collection.to_dict(), 200)
+    
+    def patch(self, id):
+        collection = Collection.query.filter_by(id=id).first()
+        if not collection:
+            return make_response({'error': 'Collection not found'}, 404)
+        data = request.get_json()
+        for key in data.keys():
+            setattr(collection, key, data[key])
+        db.session.add(collection)
+        db.session.commit()
+        return make_response(collection.to_dict(), 200)
+    
+    def delete(self, id):
+        collection = Collection.query.filter_by(id=id).first()
+        if not collection:
+            return make_response({'error': 'Collection not found'}, 404)
+        db.session.delete(collection)
+        db.session.commit()
+        return make_response({'message': 'Collection deleted'}, 204)
 api.add_resource(CollectionById, '/collections/<int:id>')
 
 class Flashcards(Resource):
@@ -53,6 +111,18 @@ class Flashcards(Resource):
         flashcards = Flashcard.query.all()
         flashcards_dict_list = [flashcard.to_dict() for flashcard in flashcards]
         return make_response(flashcards_dict_list, 200)
+    
+    def post(self):
+        data = request.get_json()
+        flashcard = Flashcard(
+            question = data['question'],
+            answer = data['answer'],
+            user_id = data['user_id'],
+            collection_id = data['collection_id']
+        )
+        db.session.add(flashcard)
+        db.session.commit()
+        return make_response(flashcard.to_dict(), 201)
 api.add_resource(Flashcards, '/flashcards')
 
 class FlashcardsById(Resource):
@@ -61,6 +131,25 @@ class FlashcardsById(Resource):
         if not flashcard:
             return make_response({'error': 'Flashcard not found'}, 404)
         return make_response(flashcard.to_dict(), 200)
+    
+    def patch(self, id):
+        flashcard = Flashcard.query.filter_by(id=id).first()
+        if not flashcard:
+            return make_response({'error': 'Flashcard not found'}, 404)
+        data = request.get_json()
+        for key in data.keys():
+            setattr(flashcard, key, data[key])
+        db.session.add(flashcard)
+        db.session.commit()
+        return make_response(flashcard.to_dict(), 200)
+    
+    def delete(self, id):
+        flashcard = Flashcard.query.filter_by(id=id).first()
+        if not flashcard:
+            return make_response({'error', 'Flashcard not found'}, 404)
+        db.session.delete(flashcard)
+        db.session.commit()
+        return make_response({'message': 'Flashcard deleted'})
 api.add_resource(FlashcardsById, '/flashcards/<int:id>')
 
 if __name__ == '__main__':
